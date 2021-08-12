029894260db50f422168ba3de3538df146448198
rodrigo-nearsoft <rsanchez@nearsoft.com>
2021-08-12 12:37:57 -0700

    Created Main view, model and viewmodel

---
+++ Models/LcrSimulation.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace LcrSimulator
+{
+    public class LcrSimulation : INotifyPropertyChanged, IDataErrorInfo
+    {
+        private string _error;
+        private int _numberOfPlayers;
+        private int _numberOfGames;
+        private int _shortestGame;
+        private int _longestGame;
+        private double _averageGameLength;
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected bool SetProperty<T>(ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                //validator.Invoke(newValue);
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                return true;
+            }
+            return false;
+        }
+        #endregion
+
+        public int NumberOfPlayers 
+        {
+            get => _numberOfPlayers; 
+            set => SetProperty(ref _numberOfPlayers, value);
+        }
+        public int NumberOfGames
+        {
+            get => _numberOfGames;
+            set => SetProperty(ref _numberOfGames, value);
+        }
+        public int ShortestGame
+        {
+            get => _shortestGame;
+            set => SetProperty(ref _shortestGame, value);
+        }
+        public int LongestGame
+        {
+            get => _longestGame;
+            set => SetProperty(ref _longestGame, value);
+        }
+        public double AverageGameLength
+        {
+            get => _averageGameLength;
+            set => SetProperty(ref _averageGameLength, value);
+        }
+        public string Error
+        {
+            get => _error;
+            set => SetProperty(ref _error, value);
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string error = string.Empty;
+                switch (columnName)
+                {
+                    case nameof(NumberOfPlayers):
+                        if(!ValidateNumberOfPlayers()) 
+                            error = "Number of players has to be 3 or greater.";
+                        break;
+                    case nameof(NumberOfGames):
+                        if (!ValidateNumberOfGames())
+                            error = "Number of players has to be 1 or greater.";
+                        break;
+                }
+                return error;
+            }
+        }
+
+        private bool ValidateNumberOfPlayers()
+        {
+            return _numberOfPlayers > 2;
+        }
+
+        private bool ValidateNumberOfGames()
+        {
+            return _numberOfGames > 0;
+        }
+
+        public bool ValidateData()
+        {
+            return ValidateNumberOfGames() && ValidateNumberOfPlayers();
+        }
+
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace LcrSimulator
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private LcrSimulation _lcrSimulation;
+        public MainWindowViewModel()
+        {
+            
+            LcrSimulation = new LcrSimulation() { NumberOfPlayers = 3, NumberOfGames = 1};
+            RunCommand = new RelayCommand<object>(RunCommandExecute, RunCommandCanExecute);
+        }
+
+        public LcrSimulation LcrSimulation
+        {
+            get => _lcrSimulation;
+            set => _lcrSimulation = value;
+        }
+
+        private ICommand _runCommand;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand RunCommand
+        {
+            get
+            {
+                if (_runCommand == null)
+                    _runCommand = new RelayCommand<object>(RunCommandExecute, RunCommandCanExecute);
+                return _runCommand;
+            }
+            set 
+            {
+                _runCommand = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(RunCommand)));
+            }
+        }
+
+        private bool RunCommandCanExecute(object parameter)
+        {
+            // Only allow execute if parameter has data and ValidateData is true
+            return parameter != null && ((LcrSimulation)parameter).ValidateData();
+        }
+
+        public void RunCommandExecute(object parameter)
+        {
+            var param = (LcrSimulation)parameter;
+            param.ShortestGame = 10;
+            param.LongestGame = 10;
+            param.AverageGameLength = 10;
+        }
+
+        public class RelayCommand<T> : ICommand
+        {
+            #region Fields
+
+            readonly Action<T> _execute = null;
+            readonly Predicate<T> _canExecute = null;
+
+            #endregion
+
+            #region Constructors
+
+            /// <summary>
+            /// Initializes a new instance of <see cref="DelegateCommand{T}"/>.
+            /// </summary>
+            /// <param name="execute">Delegate to execute when Execute is called on the command.  This can be null to just hook up a CanExecute delegate.</param>
+            /// <remarks><seealso cref="CanExecute"/> will always return true.</remarks>
+            public RelayCommand(Action<T> execute)
+                : this(execute, null)
+            {
+            }
+
+            /// <summary>
+            /// Creates a new command.
+            /// </summary>
+            /// <param name="execute">The execution logic.</param>
+            /// <param name="canExecute">The execution status logic.</param>
+            public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+            {
+                if (execute == null)
+                    throw new ArgumentNullException("execute");
+
+                _execute = execute;
+                _canExecute = canExecute;
+            }
+
+            #endregion
+
+            #region ICommand Members
+
+            ///<summary>
+            ///Defines the method that determines whether the command can execute in its current state.
+            ///</summary>
+            ///<param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+            ///<returns>
+            ///true if this command can be executed; otherwise, false.
+            ///</returns>
+            public bool CanExecute(object parameter)
+            {
+                return _canExecute == null ? true : _canExecute((T)parameter);
+            }
+
+            ///<summary>
+            ///Occurs when changes occur that affect whether or not the command should execute.
+            ///</summary>
+            public event EventHandler CanExecuteChanged
+            {
+                add { CommandManager.RequerySuggested += value; }
+                remove { CommandManager.RequerySuggested -= value; }
+            }
+
+            ///<summary>
+            ///Defines the method to be called when the command is invoked.
+            ///</summary>
+            ///<param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to <see langword="null" />.</param>
+            public void Execute(object parameter)
+            {
+                _execute((T)parameter);
+            }
+
+            #endregion
+        }
+    }
+}
